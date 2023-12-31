36b6f5e2436109da2428a4e38a16b8e7d8dfdf1b
Kingsley Ubah <ubahkingsley4@gmail.com>
2023-12-31 18:01:54 +0100

    feat: add footer and configure tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 
@@ -26,7 +27,7 @@ export default function RootLayout({
         <main className='font-normal'>
           <Header />
           {children}
-          {/* Footer */}
+          <Footer />
         </main>
       </body>
     </html>
