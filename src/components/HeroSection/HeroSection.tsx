b4065da4ec7b328f0e9e7249cf30947994d1ca1e
Kingsley Ubah <ubahkingsley4@gmail.com>
2024-01-02 15:53:07 +0100

    refactor: divide hero section into client and server comp

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,16 +1,9 @@
+import ClientComponent from "./ClientComponent";
+import { heading1, section2 } from "./ServerComponent";
+
 const HeroSection = () => {
   return (
-    <section className='flex px-4 items-center gap-12 container mx-auto'>
-        <div className='py-10 h-full'>
-            {/* Heading */}
-            <h1 className="font-heading mb-6">Explore our exquisite hotel</h1>
-            <p className="text-[#4a4a4a] dark:text-[#fffffea] mb-12 max-w-lg">
-                Experience an Exquisite Hotel Immersed in Rich History and Timeless Elegance
-            </p>
-
-            <button className="btn-primary">Get Started</button>
-       </div>
-  </section>
+    <ClientComponent section2={section2} heading1={heading1}/>
   )
 };
 
