a7f7dab0c9e64a8a1539dbd924be56e70ef28d22
Kingsley Ubah <ubahkingsley4@gmail.com>
2024-01-03 14:37:48 +0100

    feat: add featuredRoom section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
     console.log(session)
     return (
-        <header className="py-10 px4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+        <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
             <div className="flex items-center w-full md:2/3">
                 <Link href="/" className="font-black text-tertiary-dark">
                     Hotelzzz
